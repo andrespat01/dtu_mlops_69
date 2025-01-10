bf35ad243bca877317ce9765499932d6fb26567f
N. A. K <niklas.kjolbro@live.com>
2025-01-10 11:50:43 +0100

    Added some command line interface code

---
+++ dtu_mlops_69/src/mlops_project/model.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+class Model(nn.Module):
+    """model used"""
+    
+    def __init__(self) -> None:
+        """
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(128, 10)
+        """
+        pass
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+         
+        """
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        return self.fc1(x)
+        """
+         
+        return None
+
+if __name__ == "__main__":
+    model = Model()
---
+++ dtu_mlops_69/src/mlops_project/train.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import torch
+import typer
+from model import Model
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+    """Train a model on MNIST."""
+    model = Model().to(DEVICE)
+    
+    # Load data
+    #train_set, _ = ?
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+    
+    # Loss and optimizer
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    
+    # statistics
+    statistics = {"train_loss": [], "train_accuracy": []}
+    
+    for epoch in range(epochs):
+        model.train()
+        """
+        for i, (?, ?) in enumerate(train_dataloader):
+            optimizer.zero_grad()
+            y_pred = model(?)
+            loss = loss_fn(y_pred, target)
+            loss.backward()
+            optimizer.step()
+             
+        """
+        
+    # Save model
+    #torch.save(model.state_dict(), "models/model.pth")
